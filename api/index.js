a87a0ad35eeda481f8493d467c7b7e11bbfcfa26
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-14 12:33:29 -0300

    change folder name

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authenticateToken = require("../middleware/authenticateToken.js");
+const authenticateToken = require("../utils/authenticateToken.js");
 const moment = require("moment");
 
 //Import routes
