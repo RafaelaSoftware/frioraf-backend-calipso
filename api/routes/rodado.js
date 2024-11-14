0562f3552dd8d0083b70b0f692bdfcd96910583e
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-14 15:15:07 -0300

    add rodado

--- api/routes/rodado.js
+++ api/routes/rodado.js
@@ -2,7 +2,16 @@ const { executeQuery, addFilter } = require("../../utils/db.js");
 
 const getRodado = async (req, res) => {
   try {
-    const data = [];
+    const { filtro } = req.query;
+    let query = `SELECT id, descripcion FROM vp_os_rodado`;
+
+    let filter = "";
+    if (filtro) filter = addFilter(filter, "descripcion", filtro, "string");
+
+    query += filter;
+    query += ` ORDER BY DESCRIPCION`;
+    const data = await executeQuery(query);
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
