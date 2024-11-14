bd347031b99cb82c02edc15e07c09f313ebf3487
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-14 11:37:32 -0300

    send empty data

--- api/routes/cliente.js
+++ api/routes/cliente.js
@@ -2,32 +2,7 @@ const getCliente = async (req, res) => {
   try {
     let { filtro } = req.query;
 
-    const data = [
-      {
-        id: 1,
-        nome: "Fulano",
-        idade: 30,
-        email: "",
-      },
-      {
-        id: 2,
-        nome: "Ciclano",
-        idade: 25,
-        email: "",
-      },
-      {
-        id: 3,
-        nome: "Beltrano",
-        idade: 40,
-        email: "",
-      },
-      {
-        id: 4,
-        nome: "João",
-        idade: 35,
-        email: "",
-      },
-    ];
+    const data = [];
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
