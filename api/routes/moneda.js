8b6e99a829377b4691a119196d93853f89652d0f
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-14 15:38:45 -0300

    add moneda

--- api/routes/moneda.js
+++ api/routes/moneda.js
@@ -2,7 +2,15 @@ const { executeQuery, addFilter } = require("../../utils/db.js");
 
 const getMoneda = async (req, res) => {
   try {
-    const data = [];
+    const {} = req.query;
+    let query = `select id, nombre as descripcion, simbolo from v_moneda`;
+
+    let filter = "";
+
+    query += filter;
+    query += ` ORDER BY nombre`;
+    const data = await executeQuery(query);
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
