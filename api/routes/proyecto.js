c6290c040827badd7bf071dfc16aeb63dad8e5b8
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-14 14:26:36 -0300

    add proyecto

--- api/routes/proyecto.js
+++ api/routes/proyecto.js
@@ -2,7 +2,17 @@ const { executeQuery, addFilter } = require("../../utils/db.js");
 
 const getProyecto = async (req, res) => {
   try {
-    const data = [];
+    const { cliente, filtro } = req.query;
+    let query = `SELECT id, codigo as descripcion, cliente_id, cliente_n FROM vp_os_proyecto`;
+
+    let filter = "";
+    if (cliente) filter = addFilter(filter, "cliente_id", cliente, "uuid");
+    if (filtro) filter = addFilter(filter, "codigo", filtro, "string");
+
+    query += filter;
+    query += ` ORDER BY CODIGO`;
+    const data = await executeQuery(query);
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
