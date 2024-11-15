8649b0eb016ca30e86293226c8d9d8682c76b538
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-15 12:07:47 -0300

    add saldo empleado

--- api/routes/tipoMedioPago.js
+++ api/routes/tipoMedioPago.js
@@ -2,14 +2,14 @@ const { executeQuery, addFilter } = require("../../utils/db.js");
 
 const getTipoMedioPago = async (req, res) => {
   try {
-    const { usuario } = req.query;
+    const { empleado } = req.query;
     let query = `SELECT id, descripcion FROM vp_os_tipomediopago`;
 
     if (usuario) {
-      query += ` WHERE empleado_id = '${usuario}' OR id = 0`;
+      query += ` WHERE empleado_id = '${empleado}' OR id = 0`;
     }
     query += ` ORDER BY id`;
-
+    console.log(query);
     const data = await executeQuery(query);
 
     res.status(200).json(data);
