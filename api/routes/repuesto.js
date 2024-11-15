3ec00d55349d5a2fd611412fee79696c0dc45078
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-15 11:09:25 -0300

    change query repuesto

--- api/routes/repuesto.js
+++ api/routes/repuesto.js
@@ -3,7 +3,9 @@ const { executeQuery, addFilter } = require("../../utils/db.js");
 const getRepuesto = async (req, res) => {
   try {
     const { empleado, proyecto, filtro } = req.query;
-    let query = `SELECT producto_id as id, codigo, descripcion, cantidad, tecnico_id, ordentrabajo_id as proyecto_id FROM vp_os_repuesto_proyecto`;
+    let query = `
+      SELECT id, codigo, descripcion, cantidad, tecnico_id, ordentrabajo_id as proyecto_id 
+      FROM vp_os_repuesto_v2`;
 
     let filter = "";
     if (empleado) filter = addFilter(filter, "tecnico_id", empleado, "uuid");
@@ -13,8 +15,16 @@ const getRepuesto = async (req, res) => {
 
     query += filter;
     query += ` ORDER BY DESCRIPCION`;
+
     const data = await executeQuery(query);
 
+    //add default row for OTRO
+    data.push({
+      id: "00000000-0000-0000-0000-000000000000",
+      codigo: "0000",
+      descripcion: "OTRO",
+      cantidad: 100,
+    });
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
