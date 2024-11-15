28e328d24b54f77f44140579e96e58350c57adf0
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-15 11:25:44 -0300

    add end point

--- api/routes/tipoMedioPago.js
+++ api/routes/tipoMedioPago.js
@@ -2,8 +2,16 @@ const { executeQuery, addFilter } = require("../../utils/db.js");
 
 const getTipoMedioPago = async (req, res) => {
   try {
-    const { userID } = req.query;
-    const data = [];
+    const { usuario } = req.query;
+    let query = `SELECT id, descripcion FROM vp_os_tipomediopago`;
+
+    if (usuario) {
+      query += ` WHERE empleado_id = '${usuario}' OR id = 0`;
+    }
+    query += ` ORDER BY id`;
+
+    const data = await executeQuery(query);
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
